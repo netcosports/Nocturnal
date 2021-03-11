431ba81023ab5d49b9f8d6f5b8af0762d442a180
Anatoli4 <eugene_f@netcosports.com>
2021-03-11 16:53:02 +0300

    fix: changed version to branch in Sundial for SMP

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
 		dependencies: [
 			.package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "5.0.0")),
 			.package(name: "RxGesture", url: "https://github.com/RxSwiftCommunity/RxGesture.git", .upToNextMajor(from: "3.0.0")),
-			.package(name: "Sundial", url: "https://github.com/netcosports/Sundial.git", .upToNextMajor(from: "5.0.0")),
+			.package(name: "Sundial", url: "https://github.com/netcosports/Sundial.git", .branch("master")),
 			.package(name: "Astrolabe", url: "https://github.com/netcosports/Astrolabe.git", .upToNextMajor(from: "5.0.0")),
 			.package(name: "Alidade", url: "https://github.com/netcosports/Alidade.git", .branch("master"))
 		],
