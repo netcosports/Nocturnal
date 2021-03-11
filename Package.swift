a67d2025c95ccd0dd02ead1a263160fcdd8d4ba2
Anatoli4 <eugene_f@netcosports.com>
2021-03-11 15:21:18 +0300

    fix: RxGesture version in SPM

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
     ],
 		dependencies: [
 			.package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "5.0.0")),
-			.package(name: "RxGesture", url: "https://github.com/RxSwiftCommunity/RxGesture.git", .upToNextMajor(from: "4.0.0")),
+			.package(name: "RxGesture", url: "https://github.com/RxSwiftCommunity/RxGesture.git", .upToNextMajor(from: "3.0.0")),
 			.package(name: "Sundial", url: "https://github.com/netcosports/Sundial.git", .upToNextMajor(from: "5.0.0")),
 			.package(name: "Astrolabe", url: "https://github.com/netcosports/Astrolabe.git", .upToNextMajor(from: "5.0.0")),
 			.package(name: "AlidadeUI", url: "https://github.com/netcosports/Alidade.git", .branch("master"))
