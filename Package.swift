8799a65eac10edc44023f3d20d78f136b3f13491
Anatoli4 <eugene_f@netcosports.com>
2021-05-25 12:16:51 +0300

    chore(spm): changed Sundial and Alidade version to stable

--- Package.swift
+++ Package.swift
@@ -13,9 +13,9 @@ let package = Package(
 		dependencies: [
 			.package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "5.0.0")),
 			.package(name: "RxGesture", url: "https://github.com/RxSwiftCommunity/RxGesture.git", .upToNextMajor(from: "3.0.0")),
-			.package(name: "Sundial", url: "https://github.com/netcosports/Sundial.git", .branch("master")),
+			.package(name: "Sundial", url: "https://github.com/netcosports/Sundial.git", .upToNextMajor(from: "5.1.22")),
 			.package(name: "Astrolabe", url: "https://github.com/netcosports/Astrolabe.git", .upToNextMajor(from: "5.0.0")),
-			.package(name: "Alidade", url: "https://github.com/netcosports/Alidade.git", .branch("master"))
+			.package(name: "Alidade", url: "https://github.com/netcosports/Alidade.git", .upToNextMajor(from: "5.1.9"))
 		],
     targets: [
 			.target(name: "NocturnalCore",
