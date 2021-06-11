e5dd9c88eaf61450fbcae452e730e2be1a301fec
sergei.mikhan <sergei@netcosports.com>
2021-06-11 12:34:40 +0300

    chore: update to remove cocoapods subpecs

--- Nocturnal.podspec
+++ Nocturnal.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |s|
   s.name = 'Nocturnal'
-  s.version = '0.1.0'
+  s.version = '6.0.0'
   s.summary = 'rx-based MVVM-C componets'
 
   s.homepage = 'https://github.com/netcosports/Nocturnal'
@@ -10,36 +10,16 @@ Pod::Spec.new do |s|
   }
   s.source = { :git => 'https://github.com/netcosports/Nocturnal.git', :tag => s.version.to_s }
   s.ios.deployment_target = '10.0'
-  s.swift_versions = ['5.0', '5.1']
-  s.default_subspec = 'Core'
+  s.swift_versions = ['5.0', '5.1', '5.2', '5.3']
 
-  s.subspec 'Core' do |sub|
-    sub.source_files = 'Sources/Core/*.swift'
+  s.source_files = 'Sources/**/*.swift'
 
-    sub.dependency 'RxSwift', '~> 6'
-    sub.dependency 'RxCocoa', '~> 6'
-    sub.dependency 'RxGesture'
+  s.dependency 'RxSwift', '~> 6'
+  s.dependency 'RxCocoa', '~> 6'
+  s.dependency 'RxGesture'
 
-    sub.dependency 'Astrolabe', '~> 5'
+  s.dependency 'Astrolabe', '~> 6'
+  s.dependency 'Alidade', '~> 6'
+  s.dependency 'Sundial', '~> 6'
 
-    sub.dependency 'Alidade', '~>5'
-    sub.dependency 'Alidade/Core', '~>5'
-    sub.dependency 'Alidade/UI', '~>5'
-    sub.dependency 'Alidade/Associatable', '~>5'
-    sub.dependency 'Alidade/Flowable', '~>5'
-    sub.dependency 'Alidade/FormatterPool', '~>5'
-    sub.dependency 'Alidade/String', '~>5'
-
-    sub.dependency 'Sundial', '~> 5'
-  end
-
-  s.subspec 'ContainerView' do |sub|
-    sub.source_files = 'Sources/ContainerView/*.swift'
-    sub.dependency 'Nocturnal/Core'
-  end
-
-  s.subspec 'NavigationBar' do |sub|
-    sub.source_files = 'Sources/NavigationBar/*.swift'
-    sub.dependency 'Nocturnal/Core'
-  end
 end
--- Package.swift
+++ Package.swift
@@ -3,30 +3,29 @@
 import PackageDescription
 
 let package = Package(
-    name: "Nocturnal",
-    platforms: [
-      .iOS(.v10)
-    ],
-    products: [
-			.library(name: "NocturnalCore", targets: ["NocturnalCore"]),
-    ],
-		dependencies: [
-			.package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "5.0.0")),
-			.package(name: "RxGesture", url: "https://github.com/RxSwiftCommunity/RxGesture.git", .upToNextMajor(from: "3.0.0")),
-			.package(name: "Sundial", url: "https://github.com/netcosports/Sundial.git", .branch("master")),
-			.package(name: "Astrolabe", url: "https://github.com/netcosports/Astrolabe.git", .upToNextMajor(from: "5.0.0")),
-			.package(name: "Alidade", url: "https://github.com/netcosports/Alidade.git", .branch("master"))
-		],
-    targets: [
-			.target(name: "NocturnalCore",
-							dependencies: ["RxSwift",
-														 .product(name: "RxCocoa", package: "RxSwift"),
-														 "RxGesture",
-														 "Sundial",
-														 "Astrolabe",
-														 .product(name: "AlidadeAssociatable", package: "Alidade"),
-														 .product(name: "AlidadeUI", package: "Alidade")],
-							path: "./Sources/Core"),
-    ],
-    swiftLanguageVersions: [.v5]
+  name: "Nocturnal",
+  platforms: [
+    .iOS(.v10)
+  ],
+  products: [
+    .library(name: "NocturnalCore", targets: ["NocturnalCore"]),
+  ],
+  dependencies: [
+    .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "5.0.0")),
+    .package(name: "RxGesture", url: "https://github.com/RxSwiftCommunity/RxGesture.git", .upToNextMajor(from: "3.0.0")),
+    .package(name: "Sundial", url: "https://github.com/netcosports/Sundial.git", .branch("master")),
+    .package(name: "Astrolabe", url: "https://github.com/netcosports/Astrolabe.git", .upToNextMajor(from: "5.0.0")),
+    .package(name: "Alidade", url: "https://github.com/netcosports/Alidade.git", .branch("master"))
+  ],
+  targets: [
+    .target(name: "NocturnalCore",
+            dependencies: ["RxSwift",
+                           .product(name: "RxCocoa", package: "RxSwift"),
+                           "RxGesture",
+                           "Sundial",
+                           "Astrolabe",
+                           .product(name: "Alidade", package: "Alidade")],
+            path: "./Sources/Core"),
+  ],
+  swiftLanguageVersions: [.v5]
 )
